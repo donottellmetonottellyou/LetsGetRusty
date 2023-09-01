dd2ec69d795a519670f776824b783e7f4787d44e
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2023-09-01 13:37:50 -0400

    finish lesson on supertraits On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   lessons/033_Supertraits/Cargo.toml
    	new file:   lessons/033_Supertraits/src/main.rs

---
+++ lessons/033_Supertraits/src/main.rs
@@ -0,0 +1,92 @@
+#![allow(unused)]
+
+trait Vehicle: Paint {
+    fn park(&self);
+
+    fn get_default_color() -> String {
+        "black".into()
+    }
+}
+
+trait Paint {
+    fn paint(&mut self, color: String) {
+        println!("painting object {color}");
+    }
+}
+
+struct VehicleInfo {
+    make: String,
+    model: String,
+    year: u16,
+}
+
+struct Car {
+    info: VehicleInfo,
+}
+impl Paint for Car {}
+impl Vehicle for Car {
+    fn park(&self) {
+        println!("parking car!");
+    }
+}
+
+struct Truck {
+    info: VehicleInfo,
+}
+impl Truck {
+    fn unload(&self) {
+        println!("unloading truck");
+    }
+}
+impl Paint for Truck {}
+impl Vehicle for Truck {
+    fn park(&self) {
+        println!("parking truck!")
+    }
+}
+
+struct House;
+impl Paint for House {
+    fn paint(&mut self, color: String) {
+        println!("Painting house {color}");
+    }
+}
+
+fn main() {
+    let mut car = Car {
+        info: VehicleInfo {
+            make: "Honda".into(),
+            model: "Civic".into(),
+            year: 1995,
+        },
+    };
+    let mut house = House {};
+    let mut object = create_paintable_object();
+
+    paint_red(&mut car);
+    paint_red(&mut house);
+    paint_red(&mut object);
+
+    paint_vehicle_red(&mut car);
+    // paint_vehicle_red(&mut house);
+    // paint_vehicle_red(&mut object);
+}
+
+fn paint_red<T: Paint>(object: &mut T) {
+    object.paint("red".into());
+}
+
+fn paint_red2(object: &mut impl Paint) {
+    object.paint("red".into());
+}
+
+fn paint_vehicle_red<T>(object: &mut T)
+where
+    T: Vehicle,
+{
+    object.paint("red".into());
+}
+
+fn create_paintable_object() -> impl Paint {
+    House {}
+}
