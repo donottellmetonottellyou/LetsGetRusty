caa2e897493ecffa6ec0175a94fd9b4118734f89
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2023-09-05 14:07:20 -0400

    finished lesson on rc smart pointers

---
+++ lessons/041_Rc_Smart_Pointer/src/main.rs
@@ -0,0 +1,19 @@
+#![allow(unused)]
+
+use std::rc::Rc;
+
+struct Database {}
+
+struct AuthService {
+    db: Rc<Database>,
+}
+
+struct ContentService {
+    db: Rc<Database>,
+}
+
+fn main() {
+    let db = Rc::new(Database {});
+    let auth_service = AuthService { db: db.clone() };
+    let content_service = ContentService { db: db.clone() };
+}
