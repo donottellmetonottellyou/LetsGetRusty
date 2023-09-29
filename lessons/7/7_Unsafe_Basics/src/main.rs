c66d0a88ab4cbad20a5aa7efddec15b41ef972d0
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2023-09-29 19:21:28 -0400

    finished lesson on unsafe basics

---
+++ lessons/7/7_Unsafe_Basics/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    #[allow(unused_unsafe)]
+    unsafe {
+        // 1. Dereference a raw pointer.
+        // 2. Call an unsafe function.
+        // 3. Implement an unsafe trait.
+        // 4. Access/Modify a mutable static variable.
+        // 5. Access fields of a union.
+    }
+}
